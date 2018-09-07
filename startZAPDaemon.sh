ed5016e4c8a10eaa49bf875a0b0dfe7bf128333b
thivyalakshmi <thivyae@thoughtworks.com>
2018-09-07 12:14:26 +0530

    thivya - fix ci

--- startZAPDaemon.sh
+++ startZAPDaemon.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true
-docker pull owasp/zap2docker-stable
-docker run -u zap -p 8080:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true
+java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true
+#docker pull owasp/zap2docker-stable
+#docker run -u zap -p 8080:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true
