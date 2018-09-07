366ca1681e0f14cecbf88fea06e1539c3355089f
thivyalakshmi <thivyae@thoughtworks.com>
2018-09-07 12:52:07 +0530

    thivya - changing to junit tests

--- src/test/java/Spider.java
+++ src/test/java/Spider.java
@@ -1,7 +1,12 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.junit.Test;
 import org.zaproxy.clientapi.core.ApiResponse;
 import org.zaproxy.clientapi.core.ApiResponseElement;
 import org.zaproxy.clientapi.core.ClientApi;
@@ -9,29 +14,33 @@ import org.zaproxy.clientapi.core.ClientApiException;
 
 public class Spider {
 
-	public static void main(String[] args) throws InterruptedException, ClientApiException, IOException {
-		ClientApi api = new ClientApi("localhost", 8080, "null");
-		ApiResponse resp = api.spider.scan("http://dvwa.co.uk/", null, null, null, null);
-//		api.spider.scan("http://dvwa.co.uk/", null, null, null, null);
-		String scanid;
-		int progress;
+    @Test
+    public void spiderTest() throws ClientApiException, InterruptedException, IOException {
+        ClientApi api = new ClientApi("localhost", 8080, "null");
+        ApiResponse resp = api.spider.scan("http://dvwa.co.uk/", null, null, null, null);
+        String scanid;
+        int progress;
 
-		// Generate Scan Id
-		scanid = ((ApiResponseElement) resp).getValue();
+        // Generate Scan Id
+        scanid = ((ApiResponseElement) resp).getValue();
 
-		//Progress Tracker
-		while (true) {
-			Thread.sleep(1000);
-			progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanid)).getValue());
-			System.out.println("Spider progress : " + progress + "%");
-			if (progress >= 100) {
-				break;
-			}
-		}
+        //Progress Tracker
+        while (true) {
+            Thread.sleep(1000);
+            progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanid)).getValue());
+            System.out.println("Spider progress : " + progress + "%");
+            if (progress >= 100) {
+                break;
+            }
+        }
 
-		String Report = new String(api.core.htmlreport(), StandardCharsets.UTF_8);
-		Files.write(Paths.get(System.getProperty("user.dir") + "//Report//spider.html"), Report.getBytes());
-		System.out.println("Spider complete");
-	}
+        String Report = new String(api.core.htmlreport(), StandardCharsets.UTF_8);
+        System.out.println(System.getProperty("user.dir"));
+        Path filePath = Paths.get(System.getProperty("user.dir") + "/scan-results/spider.html");
+        if (!Files.exists(filePath, LinkOption.NOFOLLOW_LINKS))
+            Files.createFile(filePath);
+        Files.write(filePath, Report.getBytes());
+        System.out.println("Spider complete");
+    }
 
 }
--- startZAPDaemon.sh
+++ startZAPDaemon.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true
+java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true &
+./gradlew clean test
+jps | grep zap-D-2018-08-28.jar | cut -d " " -f 1 | xargs kill -9
+
 #docker pull owasp/zap2docker-stable
 #docker run -u zap -p 8080:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true
