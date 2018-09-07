9265f7976f179368085565f5106336d37e8b75fe
thivyalakshmi <thivyae@thoughtworks.com>
2018-09-07 13:03:30 +0530

    thivya - create scan results folder

--- startZAPDaemon.sh
+++ startZAPDaemon.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true &
+mkdir scan-results
 ./gradlew clean test
 jps | grep zap-D-2018-08-28.jar | cut -d " " -f 1 | xargs kill -9
 
