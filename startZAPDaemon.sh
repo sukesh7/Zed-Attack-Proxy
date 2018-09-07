ff33bf8fd01e9e23915717117fd5a9ead60fcd91
thivyalakshmi <thivyae@thoughtworks.com>
2018-09-07 23:55:47 +0530

    thivya-fix ci

--- startZAPDaemon.sh
+++ startZAPDaemon.sh
@@ -2,5 +2,5 @@
 
 java -jar ZAP/zap-D-2018-08-28.jar -daemon -config api.disablekey=true &
 ./gradlew clean test
-#jps | grep zap-D-2018-08-28.jar | cut -d " " -f 1 | xargs kill -9
+jps | grep zap-D-2018-08-28.jar | cut -d " " -f 1 | xargs kill -9
 
